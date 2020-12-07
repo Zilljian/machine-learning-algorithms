7a7ff7b064b53aea8fddc4c53a1941424e47d052
Balakin Ilya Aleksandrovich <victorieuse48@gmail.com>
2020-12-07 23:24:55 +0300

    Fix gaussian classifier. Change interfaces.

--- src/main/java/ru/machine/learning/algorithms/utils/TrainTestSplit.java
+++ src/main/java/ru/machine/learning/algorithms/utils/TrainTestSplit.java
@@ -2,7 +2,7 @@ package ru.machine.learning.algorithms.utils;
 
 import io.vavr.Tuple;
 import io.vavr.Tuple4;
-import tech.tablesaw.api.IntColumn;
+import tech.tablesaw.api.DoubleColumn;
 import tech.tablesaw.api.Table;
 
 import static java.lang.String.format;
@@ -10,11 +10,11 @@ import static java.lang.String.format;
 public class TrainTestSplit {
 
     // train, test, train labels, test labels
-    public static Tuple4<Table, Table, IntColumn, IntColumn> split(Table table, String targetName, double alpha) {
+    public static Tuple4<Table, Table, DoubleColumn, DoubleColumn> split(Table table, String targetName, double alpha) {
         if (0 < alpha && alpha < 1) {
             var splitted = table.sampleSplit(alpha);
-            var trainTarget = splitted[0].intColumn(targetName);
-            var testTarget = splitted[1].intColumn(targetName);
+            var trainTarget = splitted[0].doubleColumn(targetName);
+            var testTarget = splitted[1].doubleColumn(targetName);
             return Tuple.of(
                 Table.create(splitted[0].removeColumns(targetName).columns()),
                 Table.create(splitted[1].removeColumns(targetName).columns()),
