a525d318bb771a4489c67659b85719fe9548f0eb
Balakin Ilya Aleksandrovich <victorieuse48@gmail.com>
2020-12-16 03:58:30 +0300

    Polishing.

--- src/main/java/ru/machine/learning/algorithms/model/knn/Knn.java
+++ src/main/java/ru/machine/learning/algorithms/model/knn/Knn.java
@@ -2,16 +2,12 @@ package ru.machine.learning.algorithms.model.knn;
 
 import io.vavr.Tuple;
 import io.vavr.Tuple2;
-import io.vavr.collection.LinearSeq;
 import io.vavr.collection.List;
 import io.vavr.collection.Map;
 import io.vavr.collection.Traversable;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.math3.ml.distance.CanberraDistance;
-import org.apache.commons.math3.ml.distance.ChebyshevDistance;
 import org.apache.commons.math3.ml.distance.DistanceMeasure;
 import org.apache.commons.math3.ml.distance.EuclideanDistance;
-import org.apache.commons.math3.ml.distance.ManhattanDistance;
 import ru.machine.learning.algorithms.model.Model;
 import ru.machine.learning.algorithms.model.Util;
 import tech.tablesaw.api.DoubleColumn;
@@ -23,13 +19,6 @@ import static java.util.Comparator.comparing;
 
 @RequiredArgsConstructor
 public class Knn implements Model {
-
-    enum Metric {
-        Euclidean,
-        Manhattan,
-        Canberra,
-        Chebyshev
-    }
 
     private final int k;
     private final DistanceMeasure metric;
@@ -49,15 +38,9 @@ public class Knn implements Model {
     public static Knn withParams(@Nonnull Map<String, ?> params) {
         var k = (int) params.get("k")
             .getOrElseThrow(() -> new RuntimeException("Param 'k' is expected, but doesn't exist"));
-        var metricName = (Metric) params.get("metric")
+        var metric = (Metric) params.get("metric")
             .getOrElseThrow(() -> new RuntimeException("Param 'metric' is expected, but doesn't exist"));
-        var metric = switch (metricName) {
-            case Euclidean -> new EuclideanDistance();
-            case Manhattan -> new ManhattanDistance();
-            case Canberra -> new CanberraDistance();
-            case Chebyshev -> new ChebyshevDistance();
-        };
-        return new Knn(k, metric);
+        return new Knn(k, metric.get());
     }
 
     @Override
@@ -72,16 +55,15 @@ public class Knn implements Model {
     public List<Double> predict(@Nonnull Table test) {
         var testRows = Util.toList(test);
         return testRows
-            .map(e -> Tuple.of(e, trainRowsToTarget))
-            .map(t -> t._2.map(train -> {
+            .map(r -> trainRowsToTarget.map(train -> {
                 var label = train._2;
-                var distance = calculateDistance(t._1, train._1);
+                var distance = calculateDistance(r, train._1);
                 return Tuple.of(label, distance);
             }))
             .map(this::resolveLabel);
     }
 
-    private Double resolveLabel(LinearSeq<Tuple2<Double, Double>> neighbors) {
+    private Double resolveLabel(List<Tuple2<Double, Double>> neighbors) {
         return neighbors
             .sorted(comparing(Tuple2::_2))
             .take(k)
@@ -92,12 +74,6 @@ public class Knn implements Model {
     }
 
     private double calculateDistance(List<Double> testRow, List<Double> trainRow) {
-        var testRowArray = new double[testRow.size()];
-        var trainRowArray = new double[trainRow.size()];
-        testRow.zipWithIndex()
-            .forEach(t -> testRowArray[t._2] = t._1);
-        trainRow.zipWithIndex()
-            .forEach(t -> trainRowArray[t._2] = t._1);
-        return metric.compute(testRowArray, trainRowArray);
+        return metric.compute(Util.listToArray(testRow), Util.listToArray(trainRow));
     }
 }
